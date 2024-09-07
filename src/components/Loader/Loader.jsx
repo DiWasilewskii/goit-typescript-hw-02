40bfb58eef55cd4fd56e2ea24de6276c152558f9
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-09-07 17:16:05 +0300

    up

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import { InfinitySpin } from "react-loader-spinner";
+
+export default function loader() {
+  return 
+    (<Audio
+  height="100"
+  width="100"
+  color="#4fa94d"
+  ariaLabel="audio-loading"
+  wrapperStyle={{}}
+  wrapperClass="wrapper-class"
+  visible={true}
+  />
+  );
+}
